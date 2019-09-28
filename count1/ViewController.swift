90d40e9eb76545cc1c68a05c68b1e339a54a609e
HinakoNozaki <s1711462@s.tsukuba.ac.jp>
2019-09-28 23:31:29 +0900

    カウントアプリできた

--- count1/ViewController.swift
+++ count1/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+    }
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+    }
 }
 
